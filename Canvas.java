84c111fc5719716f4d01a3bebe53f3a70809fecd
Nganha0611 <Vonamnganha0611@gmail.com>
2024-03-04 22:08:40 +0700

    init project]

---
+++ Canvas.java
@@ -0,0 +1,114 @@
+package gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class Canvas extends JPanel {
+
+	Nonogram nonogram;
+	Solution solution;
+	int SQ_WIDTH = 20;
+	int LABEL_WIDTH = 60;
+	int LABEL_HEIGHT = 70;
+
+	public Canvas() {
+	}
+
+	public void setNonogram(Nonogram nonogram) {
+		this.nonogram = nonogram;
+		solution = null;
+
+		int gridWidth = SQ_WIDTH * nonogram.getColumnHeaders().size();
+		int gridHeight = SQ_WIDTH * nonogram.getRowHeaders().size();
+
+		int panelWidth = gridWidth + LABEL_WIDTH;
+		int panelHeight = gridHeight + LABEL_HEIGHT;
+
+		setPreferredSize(new Dimension(panelWidth, panelHeight));
+		repaint();
+	}
+
+	@Override
+	public void paintComponent(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getBounds().width, getBounds().height);
+
+		if (nonogram != null) {
+
+			int xMax = LABEL_WIDTH + nonogram.getColumnHeaders().size() * SQ_WIDTH;
+			int yMax = LABEL_HEIGHT + nonogram.getRowHeaders().size() * SQ_WIDTH;
+
+			int x = LABEL_WIDTH;
+			int y = LABEL_HEIGHT;
+
+			if (solution != null) {
+				g.setColor(Color.BLACK);
+				for (int i = 0; i < nonogram.getColumnHeaders().size(); i++) {
+
+					for (int j = 0; j < nonogram.getRowHeaders().size(); j++) {
+
+						if (solution.getArray()[j][i]) {
+							g.fillRect(x + 2, y + 2, SQ_WIDTH - 3, SQ_WIDTH - 3);
+						}
+						y += SQ_WIDTH;
+					}
+
+					x += SQ_WIDTH;
+					y = LABEL_HEIGHT;
+				}
+			}
+
+			x = LABEL_WIDTH;
+			y = LABEL_HEIGHT;
+
+			g.setColor(Color.GRAY);
+			for (ArrayList<Integer> header : nonogram.getColumnHeaders()) {
+				g.setColor(Color.GRAY);
+				g.drawLine(x, 0, x, yMax);
+
+				int step = 15;
+				int yLabel = 15;
+
+				for (Integer number : header) {
+					g.drawString(number.toString(), x + 6, yLabel);
+					yLabel += step;
+				}
+
+				x += SQ_WIDTH;
+			}
+			g.drawLine(x, 0, x, yMax);
+
+			for (ArrayList<Integer> header : nonogram.getRowHeaders()) {
+				g.drawLine(0, y, xMax, y);
+				y += SQ_WIDTH;
+
+				String label = "";
+
+				for (Integer number : header) {
+					label += number.toString() + " ";
+				}
+
+				g.drawString(label, 5, y - 5);
+			}
+			g.drawLine(0, y, xMax, y);
+		}
+	}
+
+	public void setSolution(Solution solution) {
+		this.solution = solution;
+		solution.evaluate();
+		this.repaint();
+	}
+
+	public Solution getSolution() {
+		return solution;
+	}
+
+	public Nonogram getNonogram() {
+		return nonogram;
+	}
+}
---
+++ Window.java
@@ -0,0 +1,127 @@
+package gui;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Window extends JFrame implements ActionListener {
+
+	Main main;
+	final Canvas canvas;
+	final JLabel label;
+	JPanel panel, buttonPanel;
+	JButton startButton, loadButton;
+	JTextField textfield;
+	int storedFitness;
+
+	public Window(Main main) {
+		super("Nonogram Genetic Algorithm");
+		this.main = main;
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		canvas = new Canvas();
+		getContentPane().add(canvas, BorderLayout.CENTER);
+
+		panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.SOUTH);
+
+		label = new JLabel("-------- Press start to begin -------");
+		panel.add(label, BorderLayout.CENTER);
+
+		buttonPanel = new JPanel();
+		buttonPanel.setPreferredSize(new Dimension(400, 50));
+		panel.add(buttonPanel, BorderLayout.SOUTH);
+
+		startButton = new JButton("Start");
+		startButton.addActionListener(this);
+		startButton.setActionCommand("start");
+		startButton.setEnabled(false);
+		buttonPanel.add(startButton, BorderLayout.WEST);
+
+		loadButton = new JButton("Load");
+		loadButton.addActionListener(this);
+		loadButton.setActionCommand("load");
+		buttonPanel.add(loadButton, BorderLayout.CENTER);
+
+		setPreferredSize(new Dimension(600, 600));
+		pack();
+		setVisible(true);
+	}
+
+	public void setNonogram(Nonogram nonogram) {
+		canvas.setNonogram(nonogram);
+		pack();
+	}
+
+	public Nonogram getNonogram() {
+		return canvas.getNonogram();
+	}
+
+	public void setSolution(Solution solution) {
+		assert (solution.getNonogram() == canvas.getNonogram());
+		canvas.setSolution(solution);
+	}
+
+	public void setLabelText(int gen, int fitness) {
+		if (canvas.getSolution().getFitness() > storedFitness) {
+			storedFitness = canvas.getSolution().getFitness();
+		}
+
+		String s = "Generation: " + Integer.toString(gen) + "   " + "Highest fitness: "
+				+ Integer.toString(storedFitness) + "    Max Fitness: "
+				+ Integer.toString(canvas.getSolution().getMaxFitness());
+
+		label.setText(s);
+
+		pack();
+	}
+
+	public void setButton() {
+		startButton.setText("Start");
+		startButton.setActionCommand("start");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		if (event.getActionCommand().equals("start")) {
+			if (canvas.getNonogram() != null) {
+				main.startWorker(canvas.getSolution());
+				startButton.setText("Stop");
+				startButton.setActionCommand("stop");
+			}
+		} else if (event.getActionCommand().equals("stop")) {
+			main.cancelWorker();
+			setButton();
+		} else if (event.getActionCommand().equals("load")) {
+			try {
+				JFileChooser chooser = new JFileChooser();
+				chooser.setSelectedFile(new File("puzzles/" + "test1" + ".dat"));
+				int returnVal = chooser.showOpenDialog(this);
+				if (returnVal == JFileChooser.APPROVE_OPTION) {
+					// System.out.println("You chose to open this file: " +
+					chooser.getSelectedFile().getName();// );
+				}
+
+				setNonogram(Main.nonogramFromFile(chooser.getSelectedFile().getAbsolutePath()));
+
+				startButton.setEnabled(true);
+			} catch (Exception e) {
+				System.out.println(e);
+				System.out.println(e.getCause());
+				System.err.println("Problem in main method");
+			}
+
+		}
+	}
+
+}
